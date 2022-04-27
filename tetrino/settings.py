cf917c7f3665f34c0912a03d45d5005a87274826
badcompwaifu <phasedkamo@gmail.com>
2022-04-27 04:31:35 -0300

    fixing errors

--- tetrino/settings.py
+++ tetrino/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-&h_lkf7snqvzf@k3+tb^0t)u8le#u7#yvacp&8)5e%+e^x&h^7'
-SECRET_KEY = os.getenv('DJANGO-KEY')
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
